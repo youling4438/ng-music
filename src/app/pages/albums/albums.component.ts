7418ce48536aae04bff8a428db60d2853d490792
youling4438 <youling0106@163.com>
2023-06-03 19:22:40 +0800

    三级标签的点击逻辑

--- src/app/pages/albums/albums.component.ts
+++ src/app/pages/albums/albums.component.ts
@@ -1,9 +1,16 @@
 import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {AlbumArgs, AlbumService, CategoryInfo} from "../../services/apis/album.service";
-import {MetaValue, SubCategory} from "../../services/apis/types";
+import {MetaData, MetaValue, SubCategory} from "../../services/apis/types";
 import {ActivatedRoute, Router} from "@angular/router";
 import {CategoryService} from "../../services/business/category.service";
 import {withLatestFrom} from "rxjs/operators";
+
+interface CheckedMeta {
+	metaRowId: number;
+	metaRowName: string;
+	metaId: number;
+	metaName: string;
+}
 
 @Component({
 	selector: 'app-albums',
@@ -21,6 +28,7 @@ export class AlbumsComponent implements OnInit {
 		perPage: 30
 	};
 	categoryInfo: CategoryInfo;
+	checkedMetas: CheckedMeta[] = [];
 
 	constructor(
 		private albumServe: AlbumService,
@@ -49,9 +57,36 @@ export class AlbumsComponent implements OnInit {
 	public changeSubCategory(subCategory?: SubCategory): void {
 		if (this.searchParams.subcategory !== subCategory?.code) {
 			this.searchParams.subcategory = subCategory?.code || '';
-			this.categoryServe.setSubCategory([subCategory.displayValue]);
+			this.categoryServe.setSubCategory([subCategory?.displayValue]);
 			this.updatePageData();
 		}
+	}
+
+	changeMeta(metaData: MetaData, metaValue: MetaValue): void {
+		this.checkedMetas.push({
+			metaId: metaValue.id,
+			metaName: metaValue.displayName,
+			metaRowId: metaData.id,
+			metaRowName: metaData.name,
+		});
+	}
+
+	unCheckMeta(meta: CheckedMeta | 'clear'): void {
+		if (meta === 'clear') {
+			this.checkedMetas = [];
+		} else {
+			const targetIndex: number = this.checkedMetas.findIndex(item => meta.metaId === item.metaId && meta.metaRowId === item.metaRowId);
+			if(targetIndex > -1) {
+				this.checkedMetas.splice(targetIndex, 1);
+			}
+		}
+	}
+
+	showMetaRow(rowName: string): boolean{
+		if(this.checkedMetas.length){
+			return this.checkedMetas.findIndex(meta => meta.metaRowName === rowName) === -1;
+		}
+		return true;
 	}
 
 	private updatePageData(): void {
