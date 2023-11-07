adcca3c0ef273e3f34075bf92e1ec9397d815f88
youling4438 <youling0106@163.com>
2023-11-07 15:49:42 +0800

    分页组件 Part 2

--- src/app/share/components/pagination/pagination.component.ts
+++ src/app/share/components/pagination/pagination.component.ts
@@ -1,10 +1,65 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
+
+type PageItemType = 'page' | 'prev' | 'next' | 'prev5' | 'next5';
+
+interface PageItem {
+	type: PageItemType;
+	disabled?: boolean;
+	checked?: boolean;
+	num?: number;
+}
 
 @Component({
 	selector: 'app-pagination',
 	templateUrl: './pagination.component.html',
-	styleUrls: ['./pagination.component.scss']
+	styleUrls: ['./pagination.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PaginationComponent {
+export class PaginationComponent implements OnInit {
+	@Input() total: number = 85;
+	@Input() pageNum: number = 1;
+	@Input() pageSize: number = 10;
+	private lastNum = 0;
+	listOfPageItem: PageItem[] = [];
 
+	constructor() {
+	}
+
+	ngOnInit(): void {
+		this.lastNum = Math.ceil(this.total / this.pageSize);
+		this.listOfPageItem = this.getListOfPageItem(this.pageNum, this.lastNum);
+		console.log('this.listOfPageItem', this.listOfPageItem);
+	}
+
+	private getListOfPageItem(pageNum: number, lastNum: number): PageItem[] {
+		const list: PageItem[] = [];
+		for (let i = 1; i <= lastNum; i++) {
+			list.push({
+				num: i,
+				type: 'page',
+				checked: i === pageNum,
+			});
+		}
+		return this.concatPrevNextBtn(list, pageNum, lastNum);
+	}
+
+	private concatPrevNextBtn(list: PageItem[], pageNum: number, lastNum: number): PageItem[] {
+		return [
+			{
+				type: 'prev',
+				disabled: pageNum === 1,
+			},
+			{
+				type: 'prev5',
+			},
+			...list,
+			{
+				type: 'next5',
+			},
+			{
+				type: 'next',
+				disabled: pageNum === lastNum,
+			},
+		]
+	}
 }
--- src/app/share/components/pagination/pagination.module.ts
+++ src/app/share/components/pagination/pagination.module.ts
@@ -1,13 +1,14 @@
 import {NgModule} from '@angular/core';
 import {PaginationComponent} from './pagination.component';
 import {DirectivesModule} from "../../directives/directives.module";
+import {NgForOf, NgSwitch, NgSwitchCase, NgSwitchDefault} from "@angular/common";
 
 
 @NgModule({
 	declarations: [
 		PaginationComponent
 	],
-	imports: [DirectivesModule],
+	imports: [DirectivesModule, NgForOf, NgSwitch, NgSwitchCase, NgSwitchDefault],
 	exports: [PaginationComponent],
 })
 export class PaginationModule {
