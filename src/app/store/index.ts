54d0b089153e6f73133b51d9f7a1c10851c0c2d1
youling4438 <youling0106@163.com>
2023-12-16 23:46:01 +0800

    Meta reducer 添加日志

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -2,12 +2,15 @@ import {NgModule} from '@angular/core';
 import {StoreModule} from "@ngrx/store";
 import {ContextStoreModule} from "./context";
 import {EffectsModule} from "@ngrx/effects";
+import {metaReducers} from "./config";
 
 
 @NgModule({
 	declarations: [],
 	imports: [
-		StoreModule.forRoot({}),
+		StoreModule.forRoot({}, {
+			metaReducers,
+		}),
 		EffectsModule.forRoot(),
 		ContextStoreModule,
 	]
