62785c0c6066352f0ade36aa725e6b284b5e7801
youling4438 <youling0106@163.com>
2023-12-23 23:33:21 +0800

    refactor code

--- src/app/store/book/action.ts
+++ src/app/store/book/action.ts
@@ -2,12 +2,6 @@ import {createActionGroup, emptyProps, props} from "@ngrx/store";
 import {Book,} from "./reducer";
 import {Update} from "@ngrx/entity";
 
-// export const addBook = createAction('[Book] Add a book', props<Book>);
-// export const updateBook = createAction('[Book] Update a book', props<Update<Book>>);
-// export const deleteBook = createAction('[Book] Delete a book', props<{ id: string }>);
-// export const clearAllBook = createAction('[Book] Clear all book');
-// export const setSelectedBook = createAction('[Book] Select a book', props<{ id: string }>);
-
 export const BookActions = createActionGroup({
 	source: 'Book',
 	events: {
