413e6e3486305dc745129308a439899c36eaca13
youling4438 <youling0106@163.com>
2023-12-26 07:59:04 +0800

    add router-store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {CoreModule} from "./core.module";
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
 	declarations: [
@@ -8,6 +9,7 @@ import {CoreModule} from "./core.module";
 	],
 	imports: [
 		CoreModule,
+		StoreRouterConnectingModule.forRoot(),
 	],
 	providers: [],
 	bootstrap: [AppComponent]
